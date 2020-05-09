5142e4f29ef4c001b55d208f24d28cf7e28b5427
Diego Guzmán <dguzman@tequilarapido.com>
2020-05-09 11:11:05 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="app">
+        Canyon Kanban
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+}
+</script>
+
+<style lang="scss">
+@use './scss/global';
+
+#app {
+    font-family: sans-serif;
+}
+</style>
