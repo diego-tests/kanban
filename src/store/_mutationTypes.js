07fb595cf5cbd7e0208e700d8f73ac160b1db5cf
Diego Guzmán <guzmand.dag@gmail.com>
2020-05-09 18:49:21 +0200

    User can move cards inside a stage and between stages closes #3 - Possibility to reorder cards within a stage closes #4 - Possibility to move a card from one stage to the other

--- src/store/_mutationTypes.js
+++ src/store/_mutationTypes.js
@@ -1,2 +1,3 @@
 export const SET_INITIAL_DATA = 'Set initial data'
+export const SET_LAST_FRONT_UPDATE = 'Set Last front update timestamp'
 export const SET_STAGE_CARDS_ORDER = 'Set Stage cards order'
--- src/store/index.js
+++ src/store/index.js
@@ -7,6 +7,7 @@ import {
 } from './_actionTypes'
 import { 
   SET_INITIAL_DATA,
+  SET_LAST_FRONT_UPDATE,
   SET_STAGE_CARDS_ORDER,
 } from './_mutationTypes'
 
@@ -15,12 +16,18 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   state: ()=>({
     stagesData: null,
+    lastFrontUpdate: new Date(),
   }),
   mutations: {
     [SET_INITIAL_DATA](state, data) {
       state.stagesData = data
     },
+    [SET_LAST_FRONT_UPDATE](state, date) {
+      state.lastFrontUpdate = date
+    },
     [SET_STAGE_CARDS_ORDER](state, { stageId, cards }) {
+      console.log(stageId)
+      console.log(cards)
       const updatedStage = state.stagesData.findIndex((item=> item.id === stageId))
 
       if (updatedStage === -1) {
@@ -46,8 +53,25 @@ export default new Vuex.Store({
       
       commit(SET_INITIAL_DATA, data)
     },
-    async [CHANGE_STAGE_CARDS_ORDER]({ commit }, payload) {
-      commit(SET_STAGE_CARDS_ORDER, payload)
+    async [CHANGE_STAGE_CARDS_ORDER]({ commit, state }, { stageId, cards }) {
+      commit(SET_STAGE_CARDS_ORDER, { stageId, cards })
+      const timeStamp = new Date().getTime
+      commit(SET_LAST_FRONT_UPDATE, timeStamp)
+
+      return
+      //if we had a backend...
+      /* eslint-disable no-unreachable */
+      axios.put(`${process.env.VUE_APP_API_ENDPOINT}/cards/:id`, { stageId, order: cards })
+        .then(res=> {
+          if (timeStamp !== state.lastFrontUpdate) {
+            //another update has been made meanwhile, we wil let that one handle the store update
+            return
+          }
+          //here we asume the backend sends a new stages array
+          commit(SET_INITIAL_DATA, res?.data?.stages)
+        })
+        .catch(err=> console.error(err))
+        /* eslint-enable no-unreachable */
     },
   },
 })
